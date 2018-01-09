bf282c52e68ec9996dd4562a7c464af77f624012
Sanjay Pandey <mnnit.sanjay@gmail.com>
2018-01-09 20:36:53 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { SettingsPage } from './../pages/settings/settings';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { QuotePage } from '../pages/quote/quote';
+import { FavouritesPage } from '../pages/favourites/favourites';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    QuotePage,
+    FavouritesPage,
+    SettingsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    QuotePage,
+    FavouritesPage,
+    SettingsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
